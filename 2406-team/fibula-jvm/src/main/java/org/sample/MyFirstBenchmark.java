dcbb5bccff61cc971b455752c6e9d1cb9b42846b
Galder Zamarreño <galder@zamarreno.com>
2024-06-13 14:32:27 +0200

    Add Fibula example to run in JVM mode

---
+++ 2406-team/fibula-jvm/src/main/java/org/sample/MyFirstBenchmark.java
@@ -0,0 +1,12 @@
+package org.sample;
+
+import org.openjdk.jmh.annotations.Benchmark;
+
+public class MyFirstBenchmark
+{
+    @Benchmark
+    public void helloWorld()
+    {
+        // Intentionally blank
+    }
+}
