a15572998bc25233f0c1a88b9fabdbadc55bc0cb
Galder Zamarreño <galder@zamarreno.com>
2024-09-09 18:19:39 +0200

    Create base for 2nd project for testing with 2 buffer impls

---
+++ 2409-netty/buffer-two/src/main/java/org/sample/netty/buffer/BufferOneBenchmark.java
@@ -0,0 +1,55 @@
+package org.sample.netty.buffer;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.PooledByteBufAllocator;
+import io.netty.util.internal.PlatformDependent;
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Fork;
+import org.openjdk.jmh.annotations.Measurement;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.TearDown;
+import org.openjdk.jmh.annotations.Warmup;
+
+import java.util.concurrent.TimeUnit;
+
+@Warmup(iterations = 5, time = 1500, timeUnit = TimeUnit.MILLISECONDS)
+@Measurement(iterations = 10, time = 1500, timeUnit = TimeUnit.MILLISECONDS)
+@Fork(3)
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.NANOSECONDS)
+@State(Scope.Thread)
+public class BufferOneBenchmark
+{
+    private ByteBuf buffer;
+
+    @Benchmark
+    public long setGetLongLE()
+    {
+        return buffer.setLongLE(0, 1).getLongLE(0);
+    }
+
+    @Benchmark
+    public ByteBuf setLongLE()
+    {
+        return buffer.setLongLE(0, 1);
+    }
+
+    @Setup
+    public void setup()
+    {
+        System.out.printf("Has unsafe: %b%n", PlatformDependent.hasUnsafe());
+        System.out.printf("Prefer direct buffer: %b%n", PlatformDependent.directBufferPreferred());
+        buffer = PooledByteBufAllocator.DEFAULT.buffer(8, 8);
+    }
+
+    @TearDown
+    public void tearDown()
+    {
+        buffer.release();
+    }
+}
