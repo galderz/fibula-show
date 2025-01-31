0527d56af270ab6174d398cf89b03df94d94e309
Galder Zamarreño <galder@zamarreno.com>
2025-01-31 07:55:09 +0100

    Add base project ahead of testing compiler control annotations

---
+++ 2502-never-inline/README.md
@@ -0,0 +1,20 @@
+# Http Header Validation
+
+## Get native results
+
+Build:
+
+```shell
+mvn package
+```
+
+Then just run:
+```shell
+java -jar target/benchmarks.jar
+```
+
+You can get trace inlining information (fails with GraalVM for DK 21 so use say GraalVM for JDK 23):
+
+```shell
+mvn package -DbuildArgs=-H:+TraceInlining
+```
---
+++ 2502-never-inline/src/main/java/org/sample/strings/CharAt.java
@@ -0,0 +1,49 @@
+package org.sample.strings;
+
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Fork;
+import org.openjdk.jmh.annotations.Measurement;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.Warmup;
+
+import java.util.concurrent.TimeUnit;
+
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.NANOSECONDS)
+@State(Scope.Thread)
+@Warmup(iterations = 10, time = 500, timeUnit = TimeUnit.MILLISECONDS)
+@Measurement(iterations = 5, time = 1000, timeUnit = TimeUnit.MILLISECONDS)
+@Fork(3)
+public class CharAt
+{
+    private String[] values;
+    private int charAtIndex;
+
+    @Setup
+    public void setup()
+    {
+        values = new String[2];
+        values[0] = "Latin1 string";
+        values[1] = "UTF-\uFF11\uFF16 string";
+        charAtIndex = 3;
+    }
+
+    @Benchmark
+    public char charAtLatin1()
+    {
+        final String strLatin1 = values[0];
+        return strLatin1.charAt(charAtIndex);
+    }
+
+    @Benchmark
+    public char charAtUtf16()
+    {
+        final String strUtf16 = values[1];
+        return strUtf16.charAt(charAtIndex);
+    }
+}
