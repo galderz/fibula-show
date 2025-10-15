dff6da9ab27d8a456dcbac9c466d05ffba3a436a
Galder Zamarreño <galder@ibm.com>
2025-10-15 11:27:37 +0200

    Automate capturing of logs

--- 2510-graalvm-summit/ce-base.sh
+++ 2510-graalvm-summit/ce-base.sh
@@ -6,9 +6,13 @@ java_home="$HOME/opt/graalvm-25"
 java="$java_home/bin/java"
 mvn="$HOME/opt/maven/bin/mvn"
 benchmark="CharAt"
+dir="logs/ce-base"
 
-JAVA_HOME=$java_home $mvn clean package
-$java -jar target/benchmarks.jar org.sample.strings.$benchmark
+mkdir -p $dir
 
-JAVA_HOME=$java_home $mvn clean package -Ddebug=true -DbuildArgs=-H:-DeleteLocalSymbols,-H:+SourceLevelDebug,-H:+TrackNodeSourcePosition,-H:+DebugCodeInfoUseSourceMappings
-$java -jar target/benchmarks.jar org.sample.strings.$benchmark -prof org.mendrugo.fibula.PerfDwarfProfiler:events=cycles
+JAVA_HOME=$java_home $mvn clean package | tee $dir/mvn-package.log
+$java -jar target/benchmarks.jar org.sample.strings.$benchmark | tee $dir/benchmark.log
+
+JAVA_HOME=$java_home $mvn clean package -Ddebug=true -DbuildArgs=-H:-DeleteLocalSymbols,-H:+SourceLevelDebug,-H:+TrackNodeSourcePosition,-H:+DebugCodeInfoUseSourceMappings | tee $dir/mvn-package-debug.log
+$java -jar target/benchmarks.jar org.sample.strings.$benchmark -prof org.mendrugo.fibula.PerfDwarfProfiler:events=cycles | tee $dir/profile.log
+perf annotate --stdio2 -i ./org.sample.strings.CharAt.latin1-AverageTime.perfbin > $dir/annotate.log
