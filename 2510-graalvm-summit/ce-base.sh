c0c5f9b59d9a26133517cb17b91f9bbc5c78d3e0
Galder Zamarreño <galder@ibm.com>
2025-10-15 10:10:11 +0200

    Fix script

--- 2510-graalvm-summit/ce-base.sh
+++ 2510-graalvm-summit/ce-base.sh
@@ -4,10 +4,11 @@ set -ex
 
 java_home="$HOME/opt/graalvm-25"
 java="$java_home/bin/java"
-mvn="JAVA_HOME=$java_home $HOME/opt/maven/bin/mvn"
+mvn="$HOME/opt/maven/bin/mvn"
+benchmark="CharAt"
 
-$mvn clean package
-$java -jar target/benchmarks.jar org.sample.strings.CharAt.charAtLatin1
+JAVA_HOME=$java_home $mvn clean package
+$java -jar target/benchmarks.jar org.sample.strings.$benchmark
 
-$mvn clean package -Ddebug=true -DbuildArgs=-H:-DeleteLocalSymbols,-H:+SourceLevelDebug,-H:+TrackNodeSourcePosition,-H:+DebugCodeInfoUseSourceMappings
-java -jar target/benchmarks.jar org.sample.strings.CharAt.charAtLatin1 -prof org.mendrugo.fibula.PerfDwarfProfiler:events=cycles
+JAVA_HOME=$java_home $mvn clean package -Ddebug=true -DbuildArgs=-H:-DeleteLocalSymbols,-H:+SourceLevelDebug,-H:+TrackNodeSourcePosition,-H:+DebugCodeInfoUseSourceMappings
+$java -jar target/benchmarks.jar org.sample.strings.$benchmark -prof org.mendrugo.fibula.PerfDwarfProfiler:events=cycles
