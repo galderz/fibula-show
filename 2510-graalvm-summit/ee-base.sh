43fcf68d1d27ca5fef738a91000a5d6830bc3c0c
Galder Zamarreño <galder@ibm.com>
2025-10-15 10:51:33 +0200

    Add script

---
+++ 2510-graalvm-summit/ee-base.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -ex
+
+java_home="$HOME/opt/ee-graalvm-25"
+java="$java_home/bin/java"
+mvn="$HOME/opt/maven/bin/mvn"
+benchmark="CharAt"
+
+JAVA_HOME=$java_home $mvn clean package
+$java -jar target/benchmarks.jar org.sample.strings.$benchmark
+
+JAVA_HOME=$java_home $mvn clean package -Ddebug=true -DbuildArgs=-H:-DeleteLocalSymbols,-H:+SourceLevelDebug,-H:+TrackNodeSourcePosition,-H:+DebugCodeInfoUseSourceMappings
+$java -jar target/benchmarks.jar org.sample.strings.$benchmark -prof org.mendrugo.fibula.PerfDwarfProfiler:events=cycles
