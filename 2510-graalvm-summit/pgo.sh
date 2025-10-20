0d3faf4a71d08c9b224f877b45392df42a540650
Galder Zamarreño <galder@ibm.com>
2025-10-20 09:34:37 +0200

    Extract struct sizes

--- 2510-graalvm-summit/pgo.sh
+++ 2510-graalvm-summit/pgo.sh
@@ -18,3 +18,4 @@ $java -jar target/benchmarks.jar org.sample.strings.$benchmark | tee $dir/benchm
 JAVA_HOME=$java_home $mvn clean package -Dpgo.perf | tee $dir/mvn-package-debug.log
 $java -jar target/benchmarks.jar org.sample.strings.$benchmark -prof org.mendrugo.fibula.PerfDwarfProfiler:events=cycles | tee $dir/profile.log
 perf annotate --stdio2 -i ./org.sample.strings.$benchmark.latin1-AverageTime.perfbin > $dir/annotate.log
+gdb -ex "ptype /o 'org.sample.strings.CharAt'" -ex "ptype /o 'java.lang.String'" -ex "ptype /o 'byte[]'" -ex quit ./target/benchmarks > $dir/structs.log
